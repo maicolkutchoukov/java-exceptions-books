bb73fcea690f2517c1bd49b5051c3d733e310864
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-21 17:36:44 +0200

    delete write and read method from main

--- src/org/exception/Library.java
+++ src/org/exception/Library.java
@@ -40,32 +40,7 @@ public class Library {
         for (Book book : books) {
             System.out.println(book);
         }
-        writer(books, "./resources/books.txt");
-        reader("./resources/books.txt");
-    }
-
-    // Metodo per scrivere i libri su un file
-    public static void writer(Book[] books, String filePath) {
-        try (FileWriter writer = new FileWriter(filePath)) {
-            for (Book book : books) {
-                writer.write(book.toString() + "\n");
-            }
-            System.out.println("Libri salvati in " + filePath);
-        } catch (IOException e) {
-            System.out.println("Errore nella scrittura: impossibile scrivere su file");
-        }
-    }
-
-    // Metodo per leggere i libri da un file
-    public static void reader(String filePath) {
-        File file = new File(filePath);
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Impossibile leggere da " + filePath);
-        }
+        handler.writer(books, "./resources/books.txt");
+        handler.reader("./resources/books.txt");
     }
 }
